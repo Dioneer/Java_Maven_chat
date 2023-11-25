b00150d7e2359412fc79cb125b5ae69e678538be
Dioneer <gulkina_em@mail.ru>
2023-11-25 21:59:08 +0300

    Socket

--- src/main/java/org/example/SocketClient.java
+++ src/main/java/org/example/SocketClient.java
@@ -4,12 +4,19 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketClient {
     public static void main(String[] args) {
         try(Socket socket = new Socket("127.0.0.1", 8081); DataInputStream dis= new DataInputStream(socket.getInputStream());
-            DataOutputStream dos= new DataOutputStream(socket.getOutputStream())){
-
+            DataOutputStream dos= new DataOutputStream(socket.getOutputStream()); Scanner scanner = new Scanner(System.in)){
+            while (true){
+                System.out.println("Print your your message: ");
+                String message = scanner.nextLine();
+                dos.writeUTF(message);
+                dos.flush();
+                System.out.println("Income: "+dis.readUTF());
+            }
         }catch(IOException ex){
             System.out.println(ex.getMessage());
         }
--- src/main/java/org/example/SocketServer.java
+++ src/main/java/org/example/SocketServer.java
@@ -1,18 +1,28 @@
 package org.example;
 
+import java.awt.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketServer {
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(8081); Socket socket = serverSocket.accept();
             DataInputStream dis= new DataInputStream(socket.getInputStream());
-            DataOutputStream dos= new DataOutputStream(socket.getOutputStream())){
-            System.out.println("Client: "+ dis.readUTF());
-            dos.writeUTF("Hi from server");
+            DataOutputStream dos= new DataOutputStream(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in)){
+            String requst = dis.readUTF();
+            while(!requst.equals("exit")){
+                System.out.println("Income: " + requst);
+                System.out.println("Enter your message: ");
+                String message = scanner.nextLine();
+                dos.writeUTF(message);
+                dos.flush();
+                requst = dis.readUTF();
+                }
         }catch (IOException ex){
             System.out.println(ex.getMessage());
         }
