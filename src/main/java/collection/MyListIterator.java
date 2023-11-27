f7da5e7014e930fc386e017c7078f3597371dea6
Dioneer <gulkina_em@mail.ru>
2023-11-27 22:21:17 +0300

    listiterator

--- src/main/java/collection/MyListIterator.java
+++ src/main/java/collection/MyListIterator.java
@@ -1,7 +1,21 @@
 package collection;
 
-public class MyListIterator {
-    public static void main(String[] args) {
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class MyListIterator {
+      public static void main(String[] args) {
+        String s = "madam";
+        List<Character> check = s.chars().mapToObj(i->(char)i).toList();
+        ListIterator<Character> listIterator = check.listIterator();
+        ListIterator<Character> listIterator1 = check.listIterator(check.size());
+        boolean flag = true;
+        while(listIterator1.hasPrevious() && listIterator.hasNext()){
+            if(listIterator.next() !=listIterator1.previous() ) {
+                flag = false;
+            break;
+            }
+        }
+        System.out.println(flag);
     }
 }
