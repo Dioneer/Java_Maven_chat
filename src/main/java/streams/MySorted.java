46058f7faee4b68ebb095ba35e6caa1cf814ae58
Dioneer <gulkina_em@mail.ru>
2023-12-14 23:59:37 +0300

    streams

--- src/main/java/streams/MySorted.java
+++ src/main/java/streams/MySorted.java
@@ -1,13 +1,12 @@
 package streams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import algorithms.SortUtils;
+
+import java.util.*;
 
 public class MySorted {
     public static void main(String[] args) {
-//        int[] arr = {3,8,4,1,2,5,6,4,110};
+        int[] arr = {3,8,4,1,2,5,6,4,110};
 //        arr = Arrays.stream(arr).sorted().toArray();
 //        System.out.println(Arrays.toString(arr));
         Student st1 = new Student("Ivan", 'm', 22);
@@ -21,6 +20,14 @@ public class MySorted {
 //        st = st.stream().sorted(Comparator.comparingInt(Student::getAge)).toList();
 //        st = st.stream().sorted((x,y)-> x.getAge()-y.getAge()).toList();
 //        System.out.println(st);
-        st.stream().filter(i->i.getSex()=='f').sorted(Comparator.comparingInt(Student::getAge)).forEach(System.out::println);
+//        st.stream().filter(i->i.getSex()=='f').sorted(Comparator.comparingInt(Student::getAge)).forEach(System.out::println);
+//        long res = Arrays.stream(arr).filter(i->i>0).count();
+//        System.out.println(res);
+        Collection collection = Arrays.asList("a1","a12","a3","a1","a13");
+        System.out.println(collection.stream().filter("a1"::equals).count());
+        System.out.println(collection.stream().findFirst().orElse(0));
+        System.out.println(collection.stream().findFirst().orElse(0));
+        System.out.println(collection.stream().skip(collection.size() - 1).findFirst().orElse("Empty"));
+        System.out.println(collection.stream().skip(collection.size() - 1).findFirst().orElse("Empty"));
     }
 }
