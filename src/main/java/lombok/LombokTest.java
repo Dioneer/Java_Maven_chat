579f8f8ff7ca257712f557899ade77a0fd337966
Dioneer <gulkina_em@mail.ru>
2023-12-15 15:48:52 +0300

    lombok

---
+++ src/main/java/lombok/LombokTest.java
@@ -0,0 +1,12 @@
+package lombok;
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class LombokTest {
+    private String login;
+    private String password;
+    private int age;
+
+}
---
+++ src/main/java/lombok/Main.java
@@ -0,0 +1,11 @@
+package lombok;
+
+public class Main {
+    public static void main(String[] args) {
+//        LombokTest lombokTest1 = new LombokTest("asd", "asdasd", 12);
+//        System.out.println(lombokTest1.getLogin());
+//        System.out.println(lombokTest1.getAge());
+//        System.out.println(lombokTest1.getPassword());
+
+    }
+}
