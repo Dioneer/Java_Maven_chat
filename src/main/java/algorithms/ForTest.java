ece9a612f6b5d9d2da6acf668953801f7910f26c
Dioneer <gulkina_em@mail.ru>
2023-12-17 00:23:00 +0300

    threads...

--- src/main/java/algorithms/ForTest.java
+++ src/main/java/algorithms/ForTest.java
@@ -6,23 +6,23 @@ public class ForTest {
     public static void main(String[] args) {
         int[] arr = {-83,-79,-77,-63,-55,-37, 30,39,41,47,48,76,96,-17,-13,-8,-1,7,9};
         int[] arr2 = {-83,-79,-77,-63,-55,-37,-17,-13,-8,-1,7,9,30,39,41,47,48,76,96};
-        directSort(arr);
+//        directSort(arr);
 //        quickSort(arr, 0, arr.length-1);
 //        binarySearch(arr2, 96);
-//        heapSort(arr);
-        System.out.println(secondMinItem(arr));
+        heapSort(arr);
+//        System.out.println(secondMinItem(arr));
     }
 
     public static void directSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             int min = i;
             for (int j = i+1; j < arr.length; j++) {
-                if(arr[min]>arr[j]){
+                if(arr[j]<arr[min]){
                     min = j;
                 }
             }
             int tempo = arr[i];
-            arr[i]=arr[min];
+            arr[i] = arr[min];
             arr[min] = tempo;
         }
         System.out.println(Arrays.toString(arr));
@@ -31,27 +31,26 @@ public class ForTest {
         int left = start;
         int right = end;
         int middle = arr[(start+end)/2];
-
         do{
-            while(arr[left] < middle){
+            while(arr[left]<middle){
                 left++;
             }
-            while(arr[right] > middle){
+            while(arr[right]>middle){
                 right--;
             }
             if(left<=right){
                 if(left<right){
                     int tempo = arr[left];
-                    arr[left]=arr[right];
+                    arr[left] = arr[right];
                     arr[right] = tempo;
                 }
                 left++;
                 right--;
             }
-        }while (left<=right);
+        }while(left<=right);
 
         if(left<end){
-            quickSort(arr, left, end);
+            quickSort(arr, left,end);
         }
         if(start<right){
             quickSort(arr, start,right);
@@ -61,60 +60,58 @@ public class ForTest {
     public static void binarySearch(int[] arr, int item){
        int left = 0;
        int right = arr.length-1;
-
-       while (left<=right){
+       while(left<=right){
            int middle = (left+right)/2;
-           if(arr[middle]<item){
-               left = middle+1;
-           }
-           if(arr[middle]>item){
-               right = middle-1;
-           }
-           if(arr[middle]==item){
+           if(arr[middle]==item) {
                System.out.println(middle);
                return;
+           }
+           if(arr[middle]>item) {
+               left = middle+1;
+           }
+           if(arr[middle]<item) {
+               right = middle-1;
            }
        }
         System.out.println(-1);
     }
     private static void heapify(int[] arr, int i, int size){
-       int left = i*2+1;
-        int right = i*2+2;
-        int largest = i;
-
-        if(left<size && arr[left]> arr[largest]){
-            largest = left;
-        }
+       int left=i*2+1;
+       int right = i*2+2;
+       int largest = i;
+       if(left<size && arr[left]> arr[largest]){
+           largest = left;
+       }
         if(right<size && arr[right]> arr[largest]){
             largest = right;
         }
-        if(largest != i){
+        if(largest!=i){
             int tempo = arr[i];
-            arr[i]=arr[largest];
+            arr[i] = arr[largest];
             arr[largest] = tempo;
-            heapify(arr, largest, size);
+            heapify(arr, largest,size);
         }
     }
     public static void heapSort(int[] arr){
         for (int i = arr.length/2-1; i >=0 ; i--) {
-            heapify(arr, i, arr.length-1);
+            heapify(arr, i, arr.length);
         }
         for (int i = arr.length-1; i >=0 ; i--) {
             int tempo = arr[i];
-            arr[i]=arr[0];
+            arr[i] = arr[0];
             arr[0] = tempo;
-            heapify(arr, 0, i);
+            heapify(arr, 0,i);
         }
         System.out.println(Arrays.toString(arr));
     }
     public static int secondMinItem(int[] arr){
-        int min = arr[0];
+        int min = 0;
         int min2 = Integer.MAX_VALUE;
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i]< min){
-                min2=min;
-                min=arr[i];
-            }else if(arr[i]< min2 && arr[i] != min){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]<arr[min]){
+                min2 = min;
+                min = arr[i];
+            }else if (arr[i]< min2 && arr[i] != arr[min]){
                 min2 = arr[i];
             }
         }
---
+++ src/main/java/threads/MySynchro.java
@@ -0,0 +1,38 @@
+package threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MySynchro {
+    private static AtomicInteger counter = new AtomicInteger();
+
+    public AtomicInteger getCounter() {
+        return counter;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+//        for (int i = 0; i < 6; i++) {
+            pool.submit(test1());
+//            Thread.sleep(1000);
+//        }
+        pool.shutdown();
+        Runnable runnable = new Runnable() {
+            synchronized public void run(){
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        pool.submit(runnable);
+        Thread th1 = new Thread(runnable);
+        th1.start();
+        Thread th2 = new Thread(runnable);
+        th2.start();
+        Thread th3 = new Thread(runnable);
+        th3.start();
+    }
+    public static Runnable test1(){
+        Runnable runnable = () -> System.out.println(Thread.currentThread().getName() + " Counter " + counter.getAndIncrement());
+        return runnable;
+    };
+}
